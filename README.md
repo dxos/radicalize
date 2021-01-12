7a120ea8553cc839bb150514d2a721025cdf28cf
Rich Burdon <rich.burdon@gmail.com>
2021-01-12 08:24:15 -0500

    Fix comments.

--- README.md
+++ README.md
@@ -2,9 +2,39 @@
 
 Utils to migrate Github repos to Radicle.
 
-# Example
+## Example
 
 ```bash
 ▶ ./radicalize/scripts/repo-list.sh dxos
 ▶ ./radicalize/scripts/repo-migrate.sh dxos
+```
+
+## Setup
+
+```bash
+▶ brew install gh
+```
+
+Edit the configuration file:
+
+```yml
+# ~/.config/gh/config.yml
+aliases:
+    repos: |
+        !gh api --paginate graphql -f owner="$1" -f query='
+          query($owner: String!, $per_page: Int = 100, $endCursor: String) {
+            repositoryOwner(login: $owner) {
+              repositories(first: $per_page, after: $endCursor, ownerAffiliations: OWNER) {
+                nodes { nameWithOwner }
+                pageInfo { hasNextPage endCursor }
+              }
+            }
+          }
+        ' | jq -r '.data.repositoryOwner.repositories.nodes[].nameWithOwner' | sort
+```
+
+Query repos:
+
+```bash
+▶ gh repos dxos
 ```
--- scripts/repo-migrate.sh
+++ scripts/repo-migrate.sh
@@ -2,40 +2,39 @@
 
 namespace=${1:-dxos}
 input="./gen/$namespace.txt"
-dir="./projects/$namespace"
+root="./projects/$namespace"
 
 mkdir -p $dir
 
 git config --global init.defaultBranch main
 
-function clone() {
+function migrate() {
   echo ""
   echo "#"
   echo "# $1"
   echo "#"
   echo ""
 
-  # Clone
+  # Clone repo.
   git clone git@github.com:$1.git
 
   parts=(${line//\// })
   repo_dir=${parts[1]}
-
   pushd $repo_dir
 
-  # Strip git history
+  # Clean-up.
   rm -rf .git
   rm -rf .github
   rm -rf .idea
   rm -f yarn.lock
 
-  # Init git
+  # Init git.
   git init
   git add ".*"
   git add "*"
-  git commit -a -m "Initial commit"
+  git commit -a -m "Migration to Radical."
 
-  # Strip comments (npx)
+  # Clean-up comments
   npx @dxos/fu@1.0.8 strip --dir="**/+\(src\|stories\)" --replace --verbose
 
   # TODO(burdon): Lint fix (for each package).
@@ -45,12 +44,13 @@ function clone() {
   # TODO(burdon): Manually create project and output hash?
 }
 
+# Read input file.
 while IFS= read -r line
 do
   if [ ! -z "$line" ]
   then
-    pushd $dir
-    clone $line
+    pushd $root
+    migrate $line
     popd
   fi
 done < "$input"
