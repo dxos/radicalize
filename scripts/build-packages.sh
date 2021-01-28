55fdf3d2c5e427cb233a026d62c27b9599a3b404
rzadp <roopert7@gmail.com>
2021-01-28 17:49:59 +0200

    Add inconcusilvely built packages

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 succ="$(pwd)/gen/building-packages.txt"
 fail="$(pwd)/gen/failing-packages.txt"
+incon="$(pwd)/gen/inconclusive-packages.txt" # no package.json or other reason
 
 echo "" > $succ
 echo "" > $fail
@@ -16,6 +17,8 @@ do
     pushd $package > /dev/null
     (yarn --silent && yarn build) && echo $package >> $succ || echo $package >> $fail
     popd > /dev/null
+  else
+    echo $package >> $incon
   fi
 done
 
