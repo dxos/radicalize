a51e027764517729d20b963f4c69ac0c31bf8bfc
rzadp <roopert7@gmail.com>
2021-01-28 13:29:26 +0200

    Add a step to build packages

---
+++ scripts/build-packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+succ="$(pwd)/gen/building-packages.txt"
+fail="$(pwd)/gen/failing-packages.txt"
+
+echo "" > $succ
+echo "" > $fail
+
+pushd gen/packages > /dev/null
+
+for package in ./*
+do
+  if test -f "$package/package.json"; then
+    echo -e "\nBuilding $package"
+    pushd $package > /dev/null
+    (yarn --silent && yarn build) && echo $package > $succ || echo $package > $fail
+    popd > /dev/null
+  fi
+done
+
+popd > /dev/null
